8b3e3dc457ce5291e3fc4c1cb6c80c98fdadc701
Masashi Saisho <m.kawahara064@gmail.com>
2025-05-29 16:41:49 +0800

    fixed - gitignore

---
+++ .next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"oeIDD/FrcV6BbEoL5KHRrVrMlWq3ahXw2/HPknaRFOk=\"\n}"
---
+++ .next/static/chunks/app_layout_tsx_61af54._.js
@@ -0,0 +1,16 @@
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([
+    "static/chunks/app_layout_tsx_61af54._.js",
+    {},
+]);
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+  "path": "static/chunks/app_layout_tsx_61af54._.js",
+  "chunks": [
+    "static/chunks/[root of the server]__9d1267._.css",
+    "static/chunks/node_modules_next_cad65e._.js",
+    "static/chunks/node_modules_@firebase_auth_dist_esm2017_5b6d55._.js",
+    "static/chunks/node_modules_@firebase_firestore_dist_index_esm2017_c2fcaa.js",
+    "static/chunks/node_modules_0f9997._.js",
+    "static/chunks/_dd747c._.js"
+  ],
+  "source": "dynamic"
+});
