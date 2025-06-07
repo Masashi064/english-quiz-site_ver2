8350ef2ace0849c19d2d00ca53f700dd07bdbbe7
Masashi Saisho <m.kawahara064@gmail.com>
2025-06-07 15:17:29 +0800

    routine update

---
+++ .next/static/chunks/app_article_[slug]_page_tsx_2ad057._.js
@@ -0,0 +1,11 @@
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([
+    "static/chunks/app_article_[slug]_page_tsx_2ad057._.js",
+    {},
+]);
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+  "path": "static/chunks/app_article_[slug]_page_tsx_2ad057._.js",
+  "chunks": [
+    "static/chunks/_3caf3f._.js"
+  ],
+  "source": "dynamic"
+});
